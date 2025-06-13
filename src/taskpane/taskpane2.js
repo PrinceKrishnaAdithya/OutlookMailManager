5fec23cd08bd1fabed42c194f897f99d179527a8
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-13 15:20:33 +0530

    Update taskpane2.js

---
+++ src/taskpane/taskpane2.js
@@ -0,0 +1,15 @@
+console.log("DEBUG 1a");
+let token = "2";
+const formdata = new FormData();
+formdata.append("token",JSON.stringify(token))
+fetch("http://127.0.0.1:5000/receive_token", {
+    method: "POST",
+    body: formdata
+})
+.then(response => response.json())
+.then(data => {
+    console.log("✅ Response:", data);
+})
+.catch(error => {
+    console.error("❌ Error:", error);
+});
