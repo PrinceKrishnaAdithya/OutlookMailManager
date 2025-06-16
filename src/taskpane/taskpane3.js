5618c9295f0c9811e6eb7696cc1479ac364f1a3e
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-16 15:35:26 +0530

    Update taskpane3.js

--- src/taskpane/taskpane3.js
+++ src/taskpane/taskpane3.js
@@ -1,15 +1,20 @@
-console.log("DEBUG 1a");
-let token = "3";
-const formdata = new FormData();
-formdata.append("token",JSON.stringify(token))
-fetch("http://127.0.0.1:5000/receive_token", {
-    method: "POST",
-    body: formdata
-})
-.then(response => response.json())
-.then(data => {
-    console.log("✅ Response:", data);
-})
-.catch(error => {
-    console.error("❌ Error:", error);
-});
+function runTaskpane3Logic(event) {
+    console.log("DEBUG 1a");
+    let token = "1";
+    const formdata = new FormData();
+    formdata.append("token", JSON.stringify(token));
+
+    fetch("http://127.0.0.1:5000/receive_token", {
+        method: "POST",
+        body: formdata
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Response:", data);
+        event.completed();
+    })
+    .catch(error => {
+        console.error("Error:", error);
+        event.completed();
+    });
+}
