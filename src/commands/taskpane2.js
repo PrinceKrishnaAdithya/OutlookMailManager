04eabe45b6584d311b316296e84ecb2e23c4a13d
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-18 00:06:37 +0530

    Update taskpane2.js

--- src/commands/taskpane2.js
+++ src/commands/taskpane2.js
@@ -1,5 +1,13 @@
-async function setModeProtected(event) {
-    await OfficeRuntime.storage.setItem("mail_mode", "protected");
-    console.log("Set mode to protected using OfficeRuntime.storage");
-    event.completed();
+function setModeProtected(event) {
+  OfficeRuntime.storage.setItem("mail_mode", "protected")
+    .then(() => {
+      console.log("Mail mode set to protected");
+      event.completed();
+    })
+    .catch((error) => {
+      console.error("Storage error:", error);
+      event.completed();
+    });
 }
+
+window.setModeProtected = setModeProtected;
