4cc01687bb358ea485aec6973dc4d19e6e8202d9
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-17 23:27:31 +0530

    Update taskpane2.js

--- src/commands/taskpane2.js
+++ src/commands/taskpane2.js
@@ -1,4 +1,8 @@
 function setModeProtected(event) {
-    localStorage.setItem("mail_mode", "protected");
-    event.completed();
+    return new Promise((resolve) => {
+        console.log("Setting mode to protected");
+        localStorage.setItem("mail_mode", "protected");
+        event.completed();
+        resolve();
+    });
 }
