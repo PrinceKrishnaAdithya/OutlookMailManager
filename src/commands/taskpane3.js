b736b50e6210f2ed3e8c158b360ce1485dc93889
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-18 11:15:09 +0530

    Update taskpane3.js

--- src/commands/taskpane3.js
+++ src/commands/taskpane3.js
@@ -1,20 +1,5 @@
 function runTaskpane3Logic(event) {
-    console.log("DEBUG 1a");
-    let token = "3";
-    const formdata = new FormData();
-    formdata.append("token", JSON.stringify(token));
-
-    fetch("http://127.0.0.1:5000/receive_token", {
-        method: "POST",
-        body: formdata
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(" Response:", data);
-        event.completed();
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        event.completed();
-    });
+    localStorage.setItem("mail_mode", "public");
+    console.log("Mode set to public");
+    event.completed();
 }
