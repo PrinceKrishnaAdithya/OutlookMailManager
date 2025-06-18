efc723b019b69806ae0a6c5eb5786ab3df1a2f5b
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-18 11:03:46 +0530

    testing the new localstorage function

--- src/commands/taskpane.js
+++ src/commands/taskpane.js
@@ -1,20 +1,5 @@
 function runTaskpaneLogic(event) {
-    console.log("DEBUG 1a");
-    let token = "1";
-    const formdata = new FormData();
-    formdata.append("token", JSON.stringify(token));
-
-    fetch("http://127.0.0.1:5000/receive_token", {
-        method: "POST",
-        body: formdata
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(" Response:", data);
-        event.completed();
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        event.completed();
-    });
+    localStorage.setItem("mail_mode", "private");
+    console.log("Mode set to private");
+    event.completed();
 }
