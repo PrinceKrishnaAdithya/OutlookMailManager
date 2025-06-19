3d304aab4dae7d679ba72dbf5097a1f0d72cca55
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-19 11:20:18 +0530

    update 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
-/* eslint-disable no-undef */
-
 const devCerts = require("office-addin-dev-certs");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://mailapprepo.onrender.com/"; 
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
@@ -18,7 +16,6 @@ module.exports = async (env, options) => {
     devtool: "source-map",
     entry: {
       polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      //taskpane: ["./src/taskpane/taskpane.js", "./src/taskpane/taskpane.html"],
       commands: "./src/commands/commands.js",
     },
     output: {
@@ -51,11 +48,6 @@ module.exports = async (env, options) => {
       ],
     },
     plugins: [
-      //new HtmlWebpackPlugin({
-      //  filename: "taskpane.html",
-      //  template: "./src/taskpane/taskpane.html",
-      //  chunks: ["polyfill", "taskpane"],
-      //}),
       new CopyWebpackPlugin({
         patterns: [
           {
