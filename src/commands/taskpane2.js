8a19a35e96b4899f3bd520cc5db687b310ab2463
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-24 10:52:12 +0530

    trying new code

--- src/commands/taskpane2.js
+++ src/commands/taskpane2.js
@@ -1,4 +1,8 @@
-function runTaskpane2Logic(event) {
+//This is the function called when you press the protected button
+//In your browser localstorage, it saves the value "protected" associated with the key "mail_mode"
+//in launchevent, this localstorage is accessed and the value is read
+function runTaskpane2Logic(event) 
+{
     localStorage.setItem("mail_mode", "protected");
     console.log("Mode set to protected");
     event.completed();
