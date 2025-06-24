fbe0263d7aa54cbf682c2339349b28551de1acc9
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-24 10:52:43 +0530

    trying new code

--- src/commands/taskpane3.js
+++ src/commands/taskpane3.js
@@ -1,4 +1,8 @@
-function runTaskpane3Logic(event) {
+//This is the function called when you press the public button
+//In your browser localstorage, it saves the value "public" associated with the key "mail_mode"
+//in launchevent, this localstorage is accessed and the value is read
+function runTaskpane3Logic(event) 
+{
     localStorage.setItem("mail_mode", "public");
     console.log("Mode set to public");
     event.completed();
