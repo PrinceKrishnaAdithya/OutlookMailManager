dd78c040a02f225943e49085bca6b1eae79e5790
Prince Krishna Adithya <78862316+PrinceKrishnaAdithya@users.noreply.github.com>
2025-06-24 10:51:38 +0530

    trying new code

--- src/commands/taskpane.js
+++ src/commands/taskpane.js
@@ -1,4 +1,8 @@
-function runTaskpaneLogic(event) {
+//This is the function called when you press the private button
+//In your browser localstorage, it saves the value "private" associated with the key "mail_mode"
+//in launchevent, this localstorage is accessed and the value is read
+function runTaskpaneLogic(event) 
+{
     localStorage.setItem("mail_mode", "private");
     console.log("Mode set to private");
     event.completed();
